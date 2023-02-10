909d8b1d8512d0244bb8517571d18aa65625e534
cono14 <112408901+cono14@users.noreply.github.com>
2023-02-10 10:59:56 +0100

    Update pcb.h

--- pcb.h
+++ pcb.h
@@ -1,6 +1,5 @@
 #include <list.h>
 #define MAXPROC 20
-#define pcbFree_table[MAXPROC] //non sono sicuro sia un define
 
 /*
  Funzione che inizializza la lista pcbFree in modo da contenere tutti gli elementi della pcbFree_table.
@@ -8,6 +7,7 @@
 void initPcbs(){
     INIT_LIST_HEAD(&pcbFree_h); 
     for(int i = 0; i < MAXPROC; i++){
-        list_add_tail(&pcbFree_table[i].p_next, &pcbFree_h);
+        list_add_tail(&procp[i].p_next, &pcbFree_h);
     }
 }
+
