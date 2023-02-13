53d53abdc046fc1364153cad7a005a5c4ed5bdcb
cono14 <112408901+cono14@users.noreply.github.com>
2023-02-13 17:25:09 +0100

    Add files via upload

---
+++ panda1/pcb_test.c
@@ -0,0 +1,179 @@
+#include "pcb.h"
+#include "list.h"
+#include "assert.h"
+#include "pandos_types.h"
+#include "pandos_const.h"
+#include "container_of.h"
+
+static struct list_head pcbFree_h;
+static pcb_t pcbFree_table[MAXPROC];
+
+/*
+ Funzione che inizializza la lista pcbFree in modo da contenere tutti gli elementi della pcbFree_table.
+*/
+
+void initPcbs(){
+    INIT_LIST_HEAD(&pcbFree_h); //inizializzo la nuova lista aggiungendo solo l'elemento di testa
+    for(int i = 0; i < MAXPROC; i++){
+        list_add_tail(&pcbFree_table[i].p_list, &pcbFree_h); //inserisco tutti gli elementi nella coda della 
+        //nuova lista
+    }
+}
+/*
+  Inserisce il PCB puntato da p nella lista dei PCB liberi pcbFree.
+*/
+
+void freePcb(pcb_t *p){
+ list_add_tail(&p->p_list, &pcbFree_h);
+}
+
+/*
+  Restituisce NULL se la pcbFree_h è vuota, sennò rimuove un elemento dalla pcbFree_h, inizializza tutti i campi a
+  NULL e restituisce l'elemento rimosso.
+*/
+
+pcb_t *allocPcb(){
+    if(list_empty(&pcbFree_h)){
+        return NULL;
+    } else {
+        /* Rimuovere un elemento da pcbfree_h, salvarlo e poi resistuirlo */
+        //si prende pcbFree_h.next dato che pcbFree_h rappresenta la testa di tutta la lista, noi vogliamo il primo elemento utile
+        pcb_t *tmp = container_of(&pcbFree_h.next, pcb_t, p_list); 
+        list_del(pcbFree_h.next); //stacca l'elemento dalla lista, restano da settare a NULL i puntatori di:
+        //parent, child e sib.
+        INIT_LIST_HEAD(&tmp->p_child); //dato che si tratta di due struct list_head inizializzo due liste vuote
+        INIT_LIST_HEAD(&tmp->p_sib); //ovvero setto i puntatori next e prev a null
+        tmp->p_parent = NULL;
+        return tmp;
+    }
+}
+
+/*
+  Crea una lista di PCB, inizializzandola come lista vuota.
+*/
+
+void  mkEmptyProcQ(struct list_head *head){
+    INIT_LIST_HEAD(head); //inizializza la sentinella della lista, il resto è vuoto
+}
+
+/*
+  Restituisce TRUE se la lista puntata da head è vuota, FALSE altrimenti.
+*/
+int emptyProcQ(struct list_head *head){
+if(list_empty(head)){
+    return 1;
+ } else {
+    return 0;
+ }
+}
+
+/*
+  Inserisce l’elemento puntato da p nella coda dei processi puntata da head.
+*/
+
+void insertProcQ(struct list_head* head, pcb_t *p){
+   list_add_tail(&p->p_list, head);
+}
+
+/*
+  Restituisce l’elemento di testa della coda dei processi da head, SENZA RIMUOVERLO. Ritorna NULL se 
+  la coda non ha elementi.
+*/
+
+pcb_t* headProcQ(struct list_head* head){
+    if(list_empty(head)){ //possibile miglioria: usare emptyProcQ
+      return NULL; 
+    } else {
+        pcb_t *tmp = container_of(head->next, pcb_t, p_list); //a differenza di prima in questo caso basta passare
+        //head->next dato che head è già un puntatore alla struttura
+        return tmp;
+    }
+
+}
+
+/*
+ Rimuove il primo elemento dalla coda dei processi puntata da head. Ritorna NULL se la coda è vuota.
+  Altrimenti ritorna il puntatore all’elemento rimosso dalla lista.
+*/
+
+pcb_t* removeProcQ(struct list_head* head){
+  if(emptyProcQ(head)){
+    return NULL;
+  } else {
+    pcb_t* tmp = container_of(head->next, pcb_t, p_list); //si potrebbe usare headProcQ
+    list_del(tmp);
+    return tmp;
+  }
+}
+
+
+/*
+ Rimuove il PCB puntato da p dalla coda dei processi puntata da head. Se p non è presente nella coda, 
+ restituisce NULL. (NOTA: p può trovarsi in una posizione arbitraria della coda).
+*/
+pcb_t* outProcQ(struct list_head* head, pcb_t *p){
+struct list_head* tmp; //uso tmp come puntatore ausiliario per scorrere sulla mia lista
+list_for_each(tmp, head){
+  if(tmp == &p->p_list){ //se tmp e p puntanto alla stessa cosa allora p è nella cosa di processi puntata da head
+    list_del(tmp); //rimuovo quell'elemento
+    return p; 
+  }
+}
+return NULL;
+}
+
+/*
+  Funzioni necessarie per i test.
+*/
+
+
+
+int main(){
+    initPcbs();
+
+    pcb_t *first_pcb = allocPcb();
+    if(first_pcb == NULL){
+        printf("Failed to allocate first PCB\n");
+    } else {
+        printf("Allocated first PCB: %p\n", first_pcb);
+    }
+
+    pcb_t *second_pcb = allocPcb();
+    if(second_pcb == NULL){
+        printf("Failed to allocate second PCB\n");
+    } else {
+        printf("Allocated second PCB: %p\n", second_pcb);
+    }
+
+    freePcb(first_pcb);
+    printf("Freed first PCB: %p\n", first_pcb);
+
+    pcb_t *third_pcb = allocPcb();
+    if(third_pcb == NULL){
+        printf("Failed to allocate third PCB\n");
+    } else {
+        printf("Allocated third PCB: %p\n", third_pcb);
+    }
+
+     struct list_head test_queue;
+     mkEmptyProcQ(&test_queue);
+     assert(emptyProcQ(&test_queue) == 1); // la coda deve essere vuota all'inizio
+
+     pcb_t p1, p2, p3;
+     insertProcQ(&test_queue, &p1);
+     insertProcQ(&test_queue, &p2);
+     insertProcQ(&test_queue, &p3);
+
+     assert(emptyProcQ(&test_queue) == 0); // la coda non deve essere più vuota
+     assert(headProcQ(&test_queue) == &p1); // la testa della coda deve essere &p1
+
+     pcb_t *removed = removeProcQ(&test_queue);
+     assert(removed == &p1); // deve essere rimosso &p1 dalla coda
+     assert(headProcQ(&test_queue) == &p2); // la testa della coda ora deve essere &p2
+
+     outProcQ(&test_queue, &p3);
+     assert(headProcQ(&test_queue) == &p2); // &p3 deve essere rimosso dalla coda, la testa deve ancora essere &p2
+
+     printf("Tutti i test sono passati con successo\n");
+     return 0;
+}
